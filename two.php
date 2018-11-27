a98ec6eba140c0c6caf2f0d52f947f8845f7bb5f
jinyf7510 <jinyt7510@163.com>
2018-11-27 16:20:47 +0800

    ff

--- two.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "phpqrcode/phpqrcode.php";
-$value="where are you go?";
-$errorCorrectionLevel = "L";
-$matrixPointSize = "15";
-QRcode::png($value,false,$errorCorrectionLevel,$matrixPointSize);
-?>
